aadb48afe5bb77a6b97ab2c6e8a3e0f3c58b2ca0
jrsky723 <jrsky723@LAPTOP-PM6BUMP1.localdomain>
2023-02-13 18:02:55 +0900

    s3 uploads

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,4 +1,20 @@
 import multer from "multer";
+import multerS3 from "multer-s3";
+import { S3Client } from "@aws-sdk/client-s3";
+
+const s3 = new S3Client({
+  region: "ap-northeast-1",
+  credentials: {
+    accessKeyId: process.env.AWS_ID,
+    secretAccessKey: process.env.AWS_SECRET,
+  },
+});
+
+const multerUploader = multerS3({
+  s3: s3,
+  bucket: "wetube-jrsky723",
+  acl: "public-read",
+});
 
 export const localsMiddleWare = (req, res, next) => {
   res.locals.loggedIn = Boolean(req.session.loggedIn);
@@ -30,9 +46,11 @@ export const avatarUpload = multer({
   limits: {
     fileSize: 30000000,
   },
+  storage: multerUploader,
 });
 
 export const videoUpload = multer({
   dest: "uploads/videos/",
   limits: { fileSize: 100000000 },
+  storage: multerUploader,
 });
